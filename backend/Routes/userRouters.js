64658d80ee0ffec9cd982c937fa1580bd3fe4232
puniiith25 <puniiith25@gmail.com>
2026-03-05 22:43:21 +0530

    created user router api end points

--- backend/Routes/userRouters.js
+++ backend/Routes/userRouters.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { pool } from "../Database/db.js";
-import bcrypt from "bcrypt";
+import { logoutUser, userLogin, userRegister } from "../Controllers/userControll.js";
 
 const router = express.Router();
 
-
+router.post('/register',userRegister);
+router.post('/login',userLogin);
+router.post('/logout',logoutUser);
 
 export default router;
