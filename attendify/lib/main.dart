b1eb7554443792acfe12a64eec93aba71b048596
puniiith25 <puniiith25@gmail.com>
2026-03-18 22:12:04 +0530

    ..

--- attendify/lib/main.dart
+++ attendify/lib/main.dart
@@ -8,13 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your
-  // 
-  // application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Attendify.ai',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: LoginScreen(),
